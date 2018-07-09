fb389bcb026fd89b98771ff8e32acd6afddcab26
Jimmy Brannon <user.email>
2018-07-09 16:09:06 -0500

    update env variables

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Todo = require('./Todo');
 
-mongoose.connect(`mongodb://localhost:27017/angular-todo`, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/angular-todo`, { useNewUrlParser: true });
 
 module.exports = {Todo}
 
